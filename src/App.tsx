f6b4b41a6293301cbadec5612172f94376445d08
Justin Clemons <justin.fuzzygreenstuff@gmail.com>
2025-11-07 02:38:35 -0700

    recreate increment and decrement buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import { useState } from "react";
 
 function App() {
-  return <></>;
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+  return (
+    <>
+      <div>Count is :{count}</div>
+      <div>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+      </div>
+    </>
+  );
 }
 
 export default App;
