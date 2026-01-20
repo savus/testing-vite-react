0187124f8e025bdc8e053442e58c54927b53fb90
Justin Clemons <justin.fuzzygreenstuff@gmail.com>
2026-01-20 06:42:43 -0700

    Install and initialize express and make sure requests are functioning through Postman

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,9 @@
 import type { TOmitID, TPartialUser, TUser } from "../types";
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
 
 const BASE_URL = "http://localhost:3000";
 const headers = { "Content-Type": "application/json" };
@@ -44,3 +49,37 @@ export const Requests = {
       return response.json();
     }),
 };
+
+let characters = [
+  {
+    id: 1,
+    name: "Johnny 1",
+  },
+  {
+    id: 2,
+    name: "Johnny 2",
+  },
+  {
+    id: 3,
+    name: "Johnny 3",
+  },
+  {
+    id: 4,
+    name: "Johnny 4",
+  },
+];
+
+app.get("/characters", (req, res) => {
+  res.send(characters);
+});
+
+app.get("/characters/:id", (req, res) => {
+  res.send(characters.find((char) => char.id === +req.params.id));
+});
+
+app.post("/characters", (req, res) => {
+  characters = [...characters, req.body];
+  res.status(201).send("Great success!");
+});
+
+app.listen(3000);
